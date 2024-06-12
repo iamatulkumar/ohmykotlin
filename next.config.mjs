85fe4ef17d2c6c9e3ec063a401a9c6498d3d3f42
atulkumar <iamatul9074@gmail.com>
2024-06-12 10:07:36 +0530

    [refactor]: add dotenv

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+require('dotenv').config();
 const nextConfig = {
+    env: {
+        MONGODB_URI: process.env.MONGODB_URI,
+    }
 };
 
 export default nextConfig;
